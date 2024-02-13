e9b89d268f84a2c66fc634679a7321f5ee6b5a3b
kumanboy <husandavronov13@gmail.com>
2024-02-13 11:46:48 +0500

    insertMany

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import connectDB from "./db/connectDB.js";
-import {createDoc} from "./models/Movies.js";
+import {insertMany} from "./models/Movies.js";
 
 const app = express();
 const port = process.env.PORT || 8000
@@ -8,6 +8,6 @@ const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/movi
 
 connectDB(DATABASE_URL)
 
-createDoc()
+insertMany()
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
